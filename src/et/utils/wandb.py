70459919fc7f09d0e74d8bd7333c3e749ac0a47d
Eric Yu <eyyu@mit.edu>
2024-09-20 11:26:41 -0400

    add wandb setting up + refactor slightly

---
+++ src/et/utils/wandb.py
@@ -0,0 +1,28 @@
+import time
+
+from loguru import logger
+from wandb.sdk.wandb_run import Run
+
+
+def setup_wandb_run(run: Run, postfix: str = None):
+    """
+    Set up the name of the run in wandb.
+    The name will be in the format "mm/dd-hh:mm - run_id - postfix"
+
+    Parameters
+    ----------
+    run: wandb.sdk.wandb_run.Run
+        The run object from wandb.
+    postfix: str
+        The postfix to add to the run
+
+    Returns
+    -------
+    None
+    """
+    date = time.strftime("%m/%d-%H:%M")
+    name = f"{date} - {run.id}"
+    if postfix:
+        name += f" - {postfix}"
+    run.name = name
+    logger.info(f"Run name set to {name}")
