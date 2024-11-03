8b532a7996a3f089e5f0dec960c5718e7f9a95aa
Eric Yu <eyyu@mit.edu>
2024-11-03 13:27:56 -0500

    modifications

--- src/et/utils/seed.py
+++ src/et/utils/seed.py
@@ -28,15 +28,24 @@ def set_seed(seed: int = 0, use_torch: bool = False, use_jax: bool = False) -> A
         import jax.random as jr
         return jr.key(seed)
 
-def set_env_seed(env: Env | List[Env], seed: int):
+def set_env_seed(envs: Env | List[Env], seeds: int | List[int]) -> Env | List[Env]:
     """
     Set the gymnasium env seed
     """
-    if isinstance(env, Env):
-        env = [env]
-    for _env in env:
-        _env.reset(seed=seed)
-        _env.action_space.seed(seed)
-    logger.info(f"Fixed env seed to {seed}")
-    return env
+    # Perform assertions for convenience's sake
+    if isinstance(envs, List):
+        assert isinstance(seeds, List), "Seeds must be a list if envs is a list"
+        assert len(envs) == len(seeds), f"Number of seeds {len(seeds)} must match number of envs {len(envs)}"
+        num_envs = len(envs)
+    else:
+        assert isinstance(envs, Env), "Env must be a gymnasium.Env instance"
+        assert isinstance(seeds, int), "Seeds must be an integer if envs is a single env"
+        envs, seeds = [envs], [seeds]
+        num_envs = 1
+
+    for env, seed in zip(envs, seeds):
+        env.reset(seed=seed)
+        env.action_space.seed(seed)
+    logger.info(f"Seeded envs to {seeds}")
+    return envs if num_envs > 1 else envs[0]
 
